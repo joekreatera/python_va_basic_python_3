a7e58bbb19267932734884b3968a565d90cb406a
Joemx <jose.c.mondragon@gmail.com>
2021-05-15 13:59:36 -0500

    practice 4
    challenge created

---
+++ 15_05_2021/practice4.py
@@ -0,0 +1,23 @@
+# practice 4
+
+
+"""
+El usuario va a introducir las velocidades de tres disparos, cada disparo
+consta de una velocidad en y i x
+
+La computadora coloca aleatoriamente un objetivo en un posición x, y=0- QUe es 
+la misma altura del usuario. 
+
+
+Despues de haber recibido la información la computadora deberá indicar si el usuario
+dio en el blanco o no, con una diferencia de hasta 3 unidades a la izquierda o a la derecha. 
+
+  /\
+ /  \
+/    \
+
+(vy)/g = t
+
+d = vx*2*t
+
+"""
