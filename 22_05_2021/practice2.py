c70c4e693671bd937f8ba4ed3552f1bc3d5277b1
Joemx <jose.c.mondragon@gmail.com>
2021-05-22 12:35:37 -0500

    Practice updated with exercise

--- 22_05_2021/practice2.py
+++ 22_05_2021/practice2.py
@@ -56,12 +56,13 @@ backpack_units = []
 #    print(backpack_units)
 
 do_next = True
-while do_next:
+i = 0
+while i < 5  and do_next:
+    i += 1
     add_to_backpack(backpack_items, backpack_units)
     print(backpack_items)
     print(backpack_units)
-    r = int(input("Deseas agregar mas cosas? 0) SI 1) NO"::>))
-    
+    r = int(input("Deseas agregar mas cosas? 0) SI 1) NO ::>"))
     if(r == 1):
         do_next = False
 
@@ -69,3 +70,37 @@ units_to_use = use_from_backpack(backpack_items, backpack_units)
 print(backpack_items)
 print(backpack_units)
 print(f'Used {units_to_use}')
+
+# Utilizar objetos mientras el usuario quiera
+# Validar que los objetos no se puedan agregar
+"""
+a-1
+a-2
+a-3
+Mala lista 
+[a,a,a]
+[1,2,3]
+deberia quedar
+
+[a]
+[6]
+"""
+# Hacer una batalla contra un enemigo. El enemigo es de tipo agua, fuego, aire o tierra 
+# y cuenta con una vida en cada uno
+# el usuario agregar hasta 3 elementos a su mochila
+# cada uso, va a decidir cuanto usar de cada cosa, si su inventario se acaba antes de quitarle toda la vida
+# al enemigo, entonces, pierde. 
+# EN caso contrario gana. El enemigo cuenta con una vida aleatoria entre 10 y 30, el usuario 
+# puede meter máximo 20 de cada elemento en su mochila, pero con un máximo de 50. 
+# Ver sig tabla:
+"""
+    A F W E 
+A   T M 0 M
+F   M T M 0 
+W   0 M T M
+E   M 0 M T
+0 -> sin daño
+T -> Daño total de la cantidad de unidades
+M -> Daño a la mitad de la cantidad de unidades
+
+"""
