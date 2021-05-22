ad6b6f0ed9395df3f457f14dd734fc5f34060761
Joemx <jose.c.mondragon@gmail.com>
2021-05-22 13:58:10 -0500

    Completed practice 2

--- 22_05_2021/practice2.py
+++ 22_05_2021/practice2.py
@@ -17,20 +17,37 @@ names.remove(nn)
 names.sort()
 print(names)
 """
+from random import random
 
+def getRandom(a,b):
+    return a + (b-a)*random()
+
+def getIntRandom(a,b):
+    return a + int((b-a)*random())
+    
 #
 # Generar un sistema de inventarios
 # El usuario va a meter 3 objetos en su mochila con una cantidad de repeticiones de la misma
 # El usuario usara uno de ellos requiriendo al sistema el nombre 
 # Si el sistema identifica que existe y que tiene suficientes unidades, le dara una de ellas , restando del total
 
-def add_to_backpack(bp_items, bp_units):
+def add_to_backpack(bp_items, bp_units, total_add):
     item = input("Usuario que vas a meter en la mochila?:")
-    bp_items.append(item)
-    bp_units.append(0)
     units = int(input(f"Cuantas unidades del item {item} vas a meter en la mochila:"))
-    last_value = len(bp_units)-1
-    bp_units[last_value] = units
+    units = min(50-total_add, units)
+    
+    if item in bp_items:
+        idx = bp_items.index(item)
+        units = min(units, 20-bp_units[idx] )
+        bp_units[idx] = bp_units[idx] +  units 
+    else:
+        bp_items.append(item)
+        bp_units.append(0)
+        units = min(units, 20)
+        last_value = len(bp_units)-1
+        bp_units[last_value] = units
+        
+    return units
     
 def use_from_backpack(bp_items, bp_units):
     item = input("Usuario que vas a usar de la mochila?:")
@@ -44,7 +61,7 @@ def use_from_backpack(bp_items, bp_units):
     else:
         print(f"Sin unidaes de {item}!")
         units = 0
-    return units
+    return units, item
     
 backpack_items = []
 backpack_units = []
@@ -57,22 +74,57 @@ backpack_units = []
 
 do_next = True
 i = 0
-while i < 5  and do_next:
+units_added = 0
+while i < 3  and do_next:
     i += 1
-    add_to_backpack(backpack_items, backpack_units)
+    units_added = units_added + add_to_backpack(backpack_items, backpack_units, units_added)
     print(backpack_items)
     print(backpack_units)
-    r = int(input("Deseas agregar mas cosas? 0) SI 1) NO ::>"))
-    if(r == 1):
+    if( units_added >= 50 ):
         do_next = False
+    else:
+        r = int(input("Deseas agregar mas cosas? 0) SI 1) NO ::>"))
+        if(r == 1):
+            do_next = False
 
-units_to_use = use_from_backpack(backpack_items, backpack_units)
-print(backpack_items)
-print(backpack_units)
-print(f'Used {units_to_use}')
+print("******************************************************")
+print("********************BATTLE****************************")
+print("******************************************************")
 
-# Utilizar objetos mientras el usuario quiera
-# Validar que los objetos no se puedan agregar
+# se genera el enemigo!
+types = ['fuego','agua','aire','tierra']
+enemy_type = types[ getIntRandom(0,3.9) ]
+enemy_life = getIntRandom(10,30)
+combinations = ['aireagua','aguaaire','fuegotierra','tierrafuego']
+
+total_inventory = 0
+for i in range(0, len(backpack_units) ):
+    total_inventory += backpack_units[i]
+
+do_battle = True
+while do_battle:
+    print(f'ENEMY LIFE: > {enemy_life}')
+    print(backpack_items)
+    print(backpack_units)
+    units_to_use, type = use_from_backpack(backpack_items, backpack_units)
+    total_inventory -= units_to_use
+    if( type == enemy_type):
+        enemy_life -= units_to_use
+    else:
+        crash = type+enemy_type
+        if(crash in combinations):
+            units_to_use = 0
+        else:
+            enemy_life -= units_to_use/2
+    if( enemy_life <= 0):
+        print("Ganaste!!!!!")
+        do_battle = False
+    elif total_inventory <= 0:
+        print("Perdiste")
+        do_battle = False
+
+# [X] Utilizar objetos mientras el usuario quiera
+# [X] Validar que los objetos no se puedan agregar
 """
 a-1
 a-2
@@ -85,15 +137,17 @@ deberia quedar
 [a]
 [6]
 """
-# Hacer una batalla contra un enemigo. El enemigo es de tipo agua, fuego, aire o tierra 
-# y cuenta con una vida en cada uno
-# el usuario agregar hasta 3 elementos a su mochila
-# cada uso, va a decidir cuanto usar de cada cosa, si su inventario se acaba antes de quitarle toda la vida
-# al enemigo, entonces, pierde. 
-# EN caso contrario gana. El enemigo cuenta con una vida aleatoria entre 10 y 30, el usuario 
-# puede meter máximo 20 de cada elemento en su mochila, pero con un máximo de 50. 
+# [X] Hacer una batalla contra un enemigo. El enemigo es de tipo agua, fuego, aire o tierra 
+# [X] y cuenta con una vida en cada uno
+# [X] el usuario agregar hasta 3 elementos a su mochila
+# [X]cada uso, va a decidir cuanto usar de cada cosa, si su inventario se acaba antes de quitarle toda la vida
+# [X] al enemigo, entonces, pierde. 
+# [X] EN caso contrario gana. El enemigo cuenta con una vida aleatoria entre 10 y 30, el usuario 
+# [X] puede meter máximo 20 de cada elemento en su mochila, pero con un máximo de 50. 
 # Ver sig tabla:
 """
+
+
     A F W E 
 A   T M 0 M
 F   M T M 0 
