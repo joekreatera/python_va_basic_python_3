afb405bf39b0b225ca513c91c9881eb9e47d4865
Joemx <jose.c.mondragon@gmail.com>
2021-05-29 11:27:33 -0500

    Added Points

---
+++ 29_05_2021/Point.py
@@ -0,0 +1,71 @@
+from random import random
+
+class Point:
+    def __init__(self, px = 0, py = 0):
+        self.__x = min(max(0, px),100)
+        self.__y = min(max(0, py),100)
+        
+    def __str__(self):
+        return f'{self.__x} , {self.__y}\n'
+        
+    def getX(self):
+        return self.__x
+    
+    def getY(self):
+        return self.__y
+            
+    def setX(self, num):
+        print("setting x " + str(num) )
+        self.__x = min(max(0, num),100)
+    
+    def setY(self, num):
+        self.__y = min(max(0, num),100)
+
+class ColorPoint(Point):
+    def __init__(self, px = 0, py = 0, color = None):
+        super().__init__(px=px,py=py)
+        self.__c = color
+    def getColor(self):
+        return self.__c
+    def setColor(self, color):
+        self.__c = color
+    def __str__(self):
+        return f'{self.getX()} {self.getY()} {self.__c} '
+    
+class PointList:
+    def __init__(self):
+        self.__points = []
+    
+    def append(self,point):
+        self.__points.append(point)
+    
+    def get(self, i):
+        return self.__points[i]
+    
+    def __str__(self):
+        res = ""
+        for i in self.__points:
+            res += str(i) + '\n'
+        
+        return res
+        
+p = Point(50,70)
+q = Point(-50,-70)
+q.setX(20)
+q.setY(99)
+
+r =  ColorPoint(40,50, [23,27,250])
+print(r)
+print(r.getX())
+print(r.getY())
+
+
+"""
+points = PointList()
+f = open('points.csv','w')
+for i in range(0,1000):
+    points.append( Point( random()*100 ,  random()*100 ) )
+for i in range(0,1000):
+    f.write( str(points.get(i)) )
+f.close()
+"""
