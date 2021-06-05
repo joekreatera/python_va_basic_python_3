f2488062bb82c98ffdd0d9399ffd3fc9f4112c1a
Joemx <jose.c.mondragon@gmail.com>
2021-06-05 11:33:40 -0500

    Update Creature.py

--- 05_06_2021/Creature.py
+++ 05_06_2021/Creature.py
@@ -10,16 +10,34 @@ class Creature:
         self.__magic = 0
         self.__life = 0
         self.__max_life = 0
-        self.changeDirection()
+        self.changeDirection(max_speed)
     def move(self):
         pass
-    def changeDirection():
+    def getPx(self):
+        return self.__px
+    def getPy(self):
+        return self.__py
+    def getSx(self):
+        return self.__sx
+    def getSy(self):
+        return self.__sy
+    def getLife(self):
+        return self.__life
+    def getMaxLife(self):
+        return self.__max_life
+    def getStrength(self):
+        return self.__strength
+    def getMagic(self):
+        return self.__magic
+    def changeDirection(self, max_speed):
         max_speed = max(max_speed, Creature.MIN_SPEED)
         random_speed = int(random()*(max_speed-Creature.MIN_SPEED) + Creature.MIN_SPEED)
         random_angle = 2*pi*random()
         self.__sx = int(  random_speed*cos(random_angle)  )
         self.__sy =int(  random_speed*sin(random_angle)  )
-        
+    def __str__(self):
+        return f'{self.getPx()} {self.getPy()} {self.getSx()} {self.getSy()} \
+        {self.getStrength()} {self.getMagic()} {self.getLife()} {self.getMaxLife()}'
 if __name__ == "__main__":
-    c = Creature()
+    c = Creature(10,10,10)
     print(c)
