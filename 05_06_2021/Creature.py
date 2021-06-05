861c26434436515711738eb45b30468d70ccadd7
Joemx <jose.c.mondragon@gmail.com>
2021-06-05 11:28:03 -0500

    Update Creature.py
    Updated with self call

--- 05_06_2021/Creature.py
+++ 05_06_2021/Creature.py
@@ -2,23 +2,24 @@ from random import random
 from math import pi, sin, cos
 
 class Creature:
-    
     MIN_SPEED = 2
-    
     def __init__(self, px, py, max_speed = 1):
         self.__px = px
         self.__py = py
-        
-        max_speed = max(max_speed, Creature.MIN_SPEED)
-        random_speed = int(random()*(max_speed-Creature.MIN_SPEED) + Creature.MIN_SPEED)
-        random_angle = 2*pi*random()
-        
-        self.__sx = int(  random_speed*cos(random_angle)  )
-        self.__sy =int(  random_speed*sin(random_angle)  )
         self.__strength = 0
         self.__magic = 0
         self.__life = 0
         self.__max_life = 0
-        
+        self.changeDirection()
     def move(self):
         pass
+    def changeDirection():
+        max_speed = max(max_speed, Creature.MIN_SPEED)
+        random_speed = int(random()*(max_speed-Creature.MIN_SPEED) + Creature.MIN_SPEED)
+        random_angle = 2*pi*random()
+        self.__sx = int(  random_speed*cos(random_angle)  )
+        self.__sy =int(  random_speed*sin(random_angle)  )
+        
+if __name__ == "__main__":
+    c = Creature()
+    print(c)
