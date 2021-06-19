11c823a2f129da33ab000c8609f7eaa13e2bbebc
Joemx <jose.c.mondragon@gmail.com>
2021-06-19 13:48:07 -0500

    Created pandatest
    test

--- 19_06_2021/pandaTest1.py
+++ 19_06_2021/pandaTest1.py
@@ -3,10 +3,29 @@ from direct.task import Task
 from panda3d.core import Point3
 from panda3d.core import loadPrcFileData
 
+loadPrcFileData('', 'win-size 640 480')
 
 class FirstApp(ShowBase):
     def __init__(self):
         super().__init__(self)
+        base.messenger.toggleVerbose()
+        self.taskMgr.add(self.setup, 'setup')
+        self.taskMgr.add(self.update, 'update')
+        
+    def setup(self,task):
+        
+        square = self.loader.loadModel("models/Square")
+        square.setPos(0,0,0)
+        
+        square.reparentTo(self.render)
+        
+        return Task.done
+    
+    def update(self, task):
+        self.camera.setPos(40,40,0)
+        self.camera.lookAt(0,0,0)
+        return Task.cont
+    
         
 t = FirstApp()
 t.run()
