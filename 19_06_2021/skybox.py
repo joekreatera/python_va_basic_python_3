5e519cdef0312d18f760b8442698b941598194e0
Joemx <jose.c.mondragon@gmail.com>
2021-07-03 11:15:12 -0500

    Update skybox.py
    updated

--- 19_06_2021/skybox.py
+++ 19_06_2021/skybox.py
@@ -11,27 +11,35 @@ loadPrcFileData('', 'win-size 640 480')
 class Skybox(ShowBase):
     def __init__(self):
         super().__init__(self)
-        
+        self.angle = 0
         self.taskMgr.add(self.setup, "setup")
         self.taskMgr.add(self.update, "update")
     
-    def loadPlane(self, texName, heading, pitch, roll , side, depth, height):
+    def loadPlane(self, texName, heading, pitch, roll , side, depth, height, scale):
         square = self.loader.loadModel("models/Square")
         tex1 = self.loader.loadTexture(f'models/{texName}')
         square.setTexture(tex1)
         square.reparentTo(self.render)
         square.setPos(side,depth,height)
         square.setHpr(heading, pitch, roll )
+        square.setScale(scale,scale,scale)
         
     def setup(self,task):
-        self.loadPlane('skybox_nx.jpg', 90 ,0 ,0  , -5 , 0 ,  0)
-        self.loadPlane('skybox_px.jpg', -90 ,0 ,0  , 5 ,  0  , 0)
+        sc = 200
+        d  = 1.64*sc
+        self.loadPlane('skybox_nx.jpg', 90 ,0 ,0  , -d , 0 ,  0 , sc)
+        self.loadPlane('skybox_px.jpg', -90 ,0 ,0  , d ,  0  , 0 , sc)
+        self.loadPlane('skybox_nz.jpg', 0 ,0 ,0  , 0 ,  d  , 0 , sc)
+        self.loadPlane('skybox_pz.jpg', 180 ,0 ,0  , 0 ,  -d  , 0 , sc)
         
         return Task.done
         
     def update(self, task):
-        self.camera.setPos(0,0,0)
-        self.camera.lookAt(10,0,0)
+        # self.camera.setPos(-5,5,0)
+        self.camera.setPos(100,100,0)
+        self.camera.setHpr(self.angle,0 , 0)
+        self.angle += 0.2
+        #self.camera.lookAt(0,0,0)
         return Task.cont  
 
 s = Skybox()
