0461779208d33a0b243e9e633d33739ac5c7f3dc
Joemx <jose.c.mondragon@gmail.com>
2021-07-24 13:23:33 -0500

    UPDATED Dynamic Enemy

--- starfox/DynamicEnemy.py
+++ starfox/DynamicEnemy.py
@@ -1,6 +1,15 @@
+class ENEMY_TYPE (Enum):
+    KAMIKAZE = 0
+    CHASER = 1
+    
+class ENEMY_STATE(Enum):
+    IDLE = 0
+    CHASE = 1
+    ATTACK = 2
+
 class DynamicEnemy:
     dynamic_enemy_name = "DynamicEnemy"
-    def __init__(self, world, originalEnemy , pos, ctrav , collisionHandler):
+    def __init__(self, world, originalEnemy , pos, ctrav , collisionHandler , type = ENEMY_TYPE.KAMIKAZE):
         self.gameObject = originalEnemy.copyTo(world)
         self.gameObject.setPos(world, pos )
         ctrav.addCollider( self.gameObject.find("**collision**") , collisionHandler )
@@ -8,5 +17,13 @@ class DynamicEnemy:
         self.gameObject.setPythonTag("ObjectController", self)
         self.gameObject.setName(DynamicEnemy.dynamic_enemy_name)
         
-    def update(self, world, dt , player):
+        self.type = type
+        self.state = ENEMY_TYPE.IDLE
+        
+    def updateKamikaze(self, world, dt , player):
         pass
+    
+    def update(self, world, dt , player):
+        self.gameObject.lookAt(player)
+        if( type == ENEMY_TYPE.KAMIKAZE):
+            updateKamikaze(world,dt,player)
